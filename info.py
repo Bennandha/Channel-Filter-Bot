e32e26ec3619a75388156a15e4afa49856a4d733
surya <130282500+Bennandha@users.noreply.github.com>
2023-05-08 01:02:28 -0700

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
-SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  = -1001884373226
-ADMIN        = 1350642629
-CHANNEL      = "@GreyMatter_Bots"
+API_ID       = 3363845
+API_HASH     = "8f037e1c50b23bb683fb1293932a1a9f"
+BOT_TOKEN    = "5550700960:AAG3GuJtKjQWr0nhCS9Op_eSUPOrU1UupJQ"
+SESSION      = "BQAilfuofgVnraxmmi-Z77SFgaOfUbpU94lSpTxWOoRn5dx16Xs-VzCtHnhgy18PmTCptiTUa8teplAHV_cLs8XUxrd2_XCP3E-miLZLphnOMk2RnVqKJxtJMOIDGfdOqWn9bxCXEdFaYBC2KW6M1PTf4siD3g5bYIuLnAgwShtVr52a-1XN0-q4zhcavzkJcKBF049IVzJshyV09OzW810rCY8xY4PBh82jHmFURhCD-AsnyLf2uD-OQcLt2AUfrd-wkg2xpolR3af6H6MJ1pcd__C6TjKsnvZKlZFixFAwblYSTb5B37GCih1dFIgKp_VOno6TL_djut8yTamC0_ylXlIM9QA"
+DATABASE_URI = "mongodb+srv://renamesfbot:renamesfbot@renamesfbot.vvjumza.mongodb.net/?retryWrites=true&w=majority"
+LOG_CHANNEL  = -1001840476474
+ADMIN        = 1582435573
+CHANNEL      = "@Vipmoviez"
